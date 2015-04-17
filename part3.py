e1161b65854e3c33bdbbc72be2bf90d71236be9a
f-jiang <feilanjiang@gmail.com>
2015-04-17 00:31:20 -0400

    Add code for parts 1 and 2 of tutorial

---
+++ part3.py
@@ -0,0 +1,34 @@
+from kivy.app import App
+
+from kivy.uix.scatter import Scatter
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+
+
+class TutorialApp(App):
+    def build(self):
+        b = BoxLayout(orientation="vertical")
+        t = TextInput(text="default",
+                      font_size=50,
+                      size_hint_y=None,
+                      height=200)
+        f = FloatLayout()
+        s = Scatter()
+        l = Label(text="Hello!",
+                  font_size=50)
+
+        f.add_widget(s)
+        s.add_widget(l)
+
+        b.add_widget(t)
+        b.add_widget(f)
+
+        t.bind(text=l.setter("text"))
+
+        return b
+
+
+if __name__ == "__main__":
+    TutorialApp().run()
