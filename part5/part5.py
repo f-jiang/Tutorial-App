35c5c170d36cb3ac44e93d903d37b2bfea550467
f-jiang <feilanjiang@gmail.com>
2015-04-17 12:48:18 -0400

    Complete part 5 of tutorial

--- part5/part5.py
+++ part5/part5.py
@@ -2,10 +2,14 @@ from kivy.app import App
 
 from kivy.uix.boxlayout import BoxLayout
 
+import random
+
 
 class ScatterTextWidget(BoxLayout):
-
-    pass
+    def change_label_color(self, *args):
+        color = [random.random() for i in xrange(3)] + [1]
+        label = self.ids["my_label"]
+        label.color = color
 
 
 class TutorialApp(App):
